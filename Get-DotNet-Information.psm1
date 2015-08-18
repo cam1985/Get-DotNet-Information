3c231c6bb6a70d44c382ea6fbb967e100cbf66ab
Cam Ashton <cam@msp360.com>
2015-08-18 16:13:44 +0100

    Initial Commit of Module
    Created Functions:
    Get-DotNet-LatestInstalledVersion
    Get-DotNet-LatestInstalledRelease
    Get-DotNet-InstalledVersions
    Get-DotNet-InstalledReleases
    Get-DotNet-Information - This Returns A Summary To The PowerShell
    Console

---
+++ Get-DotNet-Information.psm1
@@ -0,0 +1,40 @@
+﻿function Get-DotNet-LatestInstalledVersion {
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | 
+    Get-ItemProperty -name Version -EA 0 | 
+    Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} | 
+    Sort-Object version -Descending | 
+    Select-Object -ExpandProperty Version -First 1
+}
+function Get-DotNet-LatestInstalledRelease {
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | 
+    Get-ItemProperty -name Release -EA 0 | 
+    Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} | 
+    Sort-Object version -Descending | 
+    Select-Object -ExpandProperty Release -First 1
+}
+function Get-DotNet-InstalledVersions {
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | 
+    Get-ItemProperty -name Version -EA 0 | 
+    Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} | 
+    Sort-Object version -Descending | 
+    Select-Object -ExpandProperty Version -Unique
+}
+function Get-DotNet-InstalledReleases {
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | 
+    Get-ItemProperty -name Release -EA 0 | 
+    Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} | 
+    Sort-Object version -Descending | 
+    Select-Object -ExpandProperty Release -Unique
+}
+
+ function Get-DotNet-Information {
+    New-Object -TypeName psObject -Property @{
+        ComputerName =  $env:COMPUTERNAME 
+        PowerShellVersion = $PSVersionTable.PSVersion
+        DotNetVersions  = (Get-DotNet-InstalledVersions) -join ", "
+        DotNetReleases  = (Get-DotNet-InstalledReleases) -join ", "
+        LatestDotNetVersion = Get-DotNet-LatestInstalledVersion
+        LatestDotNetRelease = Get-DotNet-LatestInstalledRelease
+    } | select ComputerName, PowerShellVersion, DotNetVersions, DotNetReleases, LatestDotNetVersion, LatestDotNetRelease
+}
+Export-ModuleMember -Function '*'
